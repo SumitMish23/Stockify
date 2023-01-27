7525fcf44e2569a7ee347432059bf09f07f55d82
Sumit Mishra <sumit894sumit@gmail.com>
2023-01-27 19:31:06 +0530

    Added CSS for add icon

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from "./logo.svg";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
@@ -14,7 +13,7 @@ function App() {
           <Route exact path="/watchlist" element={<Watchlist />} />
         </Routes>
       </BrowserRouter>
-      </div>
+    </div>
   );
 }
 
